6c5097f7b690418821aa08328fb593281c118ad9
Alaina Varghese <alainavarghese18@gmail.com>
2023-06-27 15:11:38 -0400

    Initial Commit

---
+++ Period App/Period_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Period_AppApp.swift
+//  Period App
+//
+//  Created by Susan Varghese on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Period_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
